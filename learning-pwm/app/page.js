f36f8404317e22dc13dfedf1ae714322fae2f9cd
Pedro Victor Gomes de Araújo <132630207+Pedro-Vic3005@users.noreply.github.com>
2025-08-22 00:22:51 +0000

    Atualizaçoes do site

--- learning-pwm/app/page.js
+++ learning-pwm/app/page.js
@@ -3,16 +3,19 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div style={{ fontSize: "30px" }} className={styles.titulo}>
-      <h1>PVGA3005!</h1>
-      <img src="assets/steveimage.jpg" width="350" height="400"/>  
-      <pre>
-        Uma descrição de um aluno super dedicada
-      </pre>
-
+      <h1>Pedro Victor</h1>
+      
+      <img src="assets/pedrovictor.jpeg" width="300" height="400" />  
+      
       <p>
-        Uma descrição de um aluno bacana
+        Olá, eu sou Pedro Victor!!! <br></br>
+        Welcome!<br></br>
+        Estudo Ciências da Computação e me encontro no 5⁰ período. Estou sempre disposto 
+        à aprender coisas novas, dedicação é a minha maior qualidade! Participo do 
+        Projeto CIn Motorola na área de teste de Softwares, onde eu consigo desenvolver 
+        minhas habilidades e conhecimentos sobre essa etapa importantíssima do 
+        desenvolvimento de Software!
       </p>
-      <h2>+ um TITLE</h2>
     </div>
   );
 }
